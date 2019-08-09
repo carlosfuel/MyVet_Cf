ffe184fb9904df1cc068887246c98b01a5f41b5d
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2019-08-09 12:37:35 -0500

    hasta video 5

--- MyVet_Cf.Web/Data/Entities/Agenda.cs
+++ MyVet_Cf.Web/Data/Entities/Agenda.cs
@@ -11,7 +11,7 @@ namespace MyVet_Cf.Web.Data.Entities
         [Display(Name = "Fecha")]
         [Required(ErrorMessage = "El campo {0} obligatorio")]
         [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd H:mm tt}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         
         public string Remarks { get; set; }
@@ -20,8 +20,8 @@ namespace MyVet_Cf.Web.Data.Entities
         public bool IsAvailable { get; set; }
 
         [Display(Name = "Fecha")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd} H:mm tt}")]
-        public DateTime BornLocal => Date.ToLocalTime();
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd} HH:mm}",ApplyFormatInEditMode =true)]
+        public DateTime DateLocal => Date.ToLocalTime();
 
         //-------------------------- Relaciones----------
         public Owner Owner { get; set; }
