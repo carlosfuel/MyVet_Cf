01434734e492038abe10b1a6cd12a2c9fd6e8431
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2019-08-19 20:01:36 -0500

    Realizado hasta el video Tutorial 58 parte 13 - Fix create in Owner

--- MyVet_Cf.Web/Controllers/OwnersController.cs
+++ MyVet_Cf.Web/Controllers/OwnersController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyVet_Cf.Web.Data;
 using MyVet_Cf.Web.Data.Entities;
+using MyVet_Cf.Web.Helpers;
+using MyVet_Cf.Web.Models;
 
 namespace MyVet_Cf.Web.Controllers
 {
@@ -15,16 +17,23 @@ namespace MyVet_Cf.Web.Controllers
     public class OwnersController : Controller
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public OwnersController(DataContext context)
+        public OwnersController(DataContext context, IUserHelper userHelper)
         {
             _context = context;
+            this._userHelper = userHelper;
         }
 
         // GET: Owners
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.Owners.ToListAsync());
+            //return View(await _context.Owners.ToListAsync());
+            return View(_context.Owners
+                .Include(o => o.User)
+                .Include(o => o.Pets));
+
+
         }
 
         // GET: Owners/Details/5
@@ -36,6 +45,11 @@ namespace MyVet_Cf.Web.Controllers
             }
 
             var owner = await _context.Owners
+                .Include(o => o.User)
+                .Include(o => o.Pets)
+                .ThenInclude(p => p.PetType)
+                .Include(o => o.Pets)
+                .ThenInclude(p => p.Histories)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (owner == null)
             {
@@ -51,20 +65,52 @@ namespace MyVet_Cf.Web.Controllers
             return View();
         }
 
-        // POST: Owners/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id")] Owner owner)
+        public async Task<IActionResult> Create(AddUserViewModel model )
         {
             if (ModelState.IsValid)
             {
-                _context.Add(owner);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var user = new User
+                {
+                    Address=model.Address,
+                    Document=model.Document,
+                    Email=model.Username,
+                    FirstName=model.FirstName,
+                    LastName=model.LastName,
+                    PhoneNumber=model.PhoneNumber,
+                    UserName=model.Username                    
+                };
+
+                var response = await _userHelper.AddUserAsync(user, model.Password);
+                if (response.Succeeded)
+                {
+                    var userInDB = await _userHelper.GetUserByEmailAsync(model.Username);
+                    await _userHelper.AddUserToRoleAsync(userInDB, "Customer");
+
+                    var owner = new Owner
+                    {
+                        Agendas = new List<Agenda>(),
+                        Pets = new List<Pet>(),
+                        User=userInDB
+                    };
+
+                    _context.Owners.Add(owner);
+
+                    try
+                    {
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch (Exception er)
+                    {
+                        ModelState.AddModelError(string.Empty, er.ToString()); 
+                    }                    
+                }
+                ModelState.AddModelError(string.Empty, response.Errors.FirstOrDefault().Description);                
             }
-            return View(owner);
+            return View(model);
         }
 
         // GET: Owners/Edit/5
---
+++ MyVet_Cf.Web/Models/AddUserViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyVet_Cf.Web.Models
+{
+    public class AddUserViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Document { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Address { get; set; }
+
+        [Display(Name = "Número telefónico")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0}  debe contener entre {2} y {1} caracteres")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirme Contraseña")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "El campo {0}  debe contener entre {2} y {1} caracteres")]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
