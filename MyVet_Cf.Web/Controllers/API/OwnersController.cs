0b018f01274f8b5f04d25ef07d04ac49666a0218
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2019-09-01 15:04:12 -0500

    Proyecto Desarrollado hasta el tutorial 58 - Part 26 - Complex API.  Septiembre 1 de 2019

---
+++ MyVet_Cf.Web/Controllers/API/OwnersController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyVet_Cf.Common.Models;
+using MyVet_Cf.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet_Cf.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public OwnersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        [Route("GetOwnerByEmail")]
+        public async Task<IActionResult> GetOwner(EmailRequest emailRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            //var owner = await _dataContex.Owners
+
+                var owner = await _dataContext.Owners
+                .Include(o => o.User)
+                .Include(o => o.Pets)
+                .ThenInclude(p => p.PetType)
+                .Include(o => o.Pets)
+                .ThenInclude(p => p.Histories)
+                .ThenInclude(h => h.ServiceType)
+                .FirstOrDefaultAsync(o => o.User.UserName.ToLower() == emailRequest.Email.ToLower());
+
+            var response = new OwnerResponse
+            {
+                Id = owner.Id,
+                FirstName = owner.User.FirstName,
+                LastName = owner.User.LastName,
+                Address = owner.User.Address,
+                Document = owner.User.Document,
+                Email = owner.User.Email,
+                PhoneNumber = owner.User.PhoneNumber,
+                Pets = owner.Pets.Select(p => new PetResponse
+                {
+                    Born = p.Born,
+                    Id = p.Id,
+                    ImageUrl = p.ImageFullPath,
+                    Name = p.Name,
+                    Race = p.Race,
+                    Remarks = p.Remarks,
+                    PetType = p.PetType.Name,
+                    Histories = p.Histories.Select(h => new HistoryResponse
+                    {
+                        Date = h.Date,
+                        Description = h.Description,
+                        Id = h.Id,
+                        Remarks = h.Remarks,
+                        ServiceType = h.ServiceType.Name
+                    }).ToList()
+                }).ToList()
+            };
+
+            return Ok(response);
+        }
+    }
+
+}
