0550d70cc6a942279b2df98d939a65fe5f32af15
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2019-10-12 21:25:02 -0500

    Proyecto Desarrollado hasta el video Tutorial 58 - Part 78 - Modify profile (1/2). Octubre 12 2019.

--- MyVet_Cf.Prism/MyVet_Cf.Prism/ViewModels/ProfilePageViewModel.cs
+++ MyVet_Cf.Prism/MyVet_Cf.Prism/ViewModels/ProfilePageViewModel.cs
@@ -1,13 +1,88 @@
-﻿using Prism.Mvvm;
+﻿using MyVet_Cf.Common.Helpers;
+using MyVet_Cf.Common.Models;
+using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Navigation;
+using System.Threading.Tasks;
 
 namespace MyVet_Cf.Prism.ViewModels
 {
     public class ProfilePageViewModel : ViewModelBase
     {
-        public ProfilePageViewModel(INavigationService navigationService) : base(navigationService)
+        private bool _isRunning;
+        private bool _isEnabled;
+        private OwnerResponse _owner;
+        private DelegateCommand _saveCommand;
+
+        public ProfilePageViewModel(
+            INavigationService navigationService) : base(navigationService)
         {
             Title = "Modificar Perfil";
+            IsEnabled = true;
+            Owner = JsonConvert.DeserializeObject<OwnerResponse>(Settings.Owner);
+
+        }
+
+        //----------------------------------------------------------
+
+        public DelegateCommand SaveCommand => _saveCommand ?? (_saveCommand = new DelegateCommand(Save));
+
+        public OwnerResponse Owner
+        {
+            get => _owner;
+            set => SetProperty(ref _owner, value);
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        private async void Save()
+        {
+            var isValid = await ValidateData();
+            if (!isValid)
+            {
+                return;
+            }
+        }
+
+        private async Task<bool> ValidateData()
+        {
+            if (string.IsNullOrEmpty(Owner.Document))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debes ingresar un Documento", "Aceptar");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Owner.FirstName))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debes ingresar Los Nombres", "Aceptar");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Owner.LastName))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debes ingresar los Apellidos", "Aceptar");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Owner.Address))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Debes ingresar la Dirección", "Aceptar");
+                return false;
+            }
+
+            return true;
         }
     }
+
 }
+
