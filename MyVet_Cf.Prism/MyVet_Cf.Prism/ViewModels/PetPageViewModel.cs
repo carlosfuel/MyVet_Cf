370e96e5c6186e7ae1abd94404d3a96111b98ade
Carlos Alberto Fuel Tulcán <carlosfuel@gmail.com>
2019-10-14 15:56:58 -0500

    Proyecto Desarrollado hasta el Video tutorial 58  - Part 85 - Button Edit Pet. Octubre 14 de 2019

--- MyVet_Cf.Prism/MyVet_Cf.Prism/ViewModels/PetPageViewModel.cs
+++ MyVet_Cf.Prism/MyVet_Cf.Prism/ViewModels/PetPageViewModel.cs
@@ -1,19 +1,28 @@
-﻿using MyVet_Cf.Common.Helpers;
+﻿using System;
+using MyVet_Cf.Common.Helpers;
 using MyVet_Cf.Common.Models;
 using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace MyVet_Cf.Prism.ViewModels
 {
     public class PetPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
         private PetResponse _pet;
+        private DelegateCommand _editPetCommand;
 
         public PetPageViewModel(
             INavigationService navigationService) : base(navigationService)
         {
-            Title = "Detalles";
+            //Title = "Detalles";
+            _navigationService = navigationService;
         }
+
+        public DelegateCommand EditPetCommand =>  _editPetCommand ?? (_editPetCommand= new DelegateCommand(EditPetAsync));
+
+        
 
         public PetResponse Pet
         {
@@ -26,6 +35,15 @@ namespace MyVet_Cf.Prism.ViewModels
             base.OnNavigatedTo(parameters);
             Pet = JsonConvert.DeserializeObject<PetResponse>(Settings.Pet);
             
+        }
+
+        private async void EditPetAsync()
+        {
+            var parametros = new NavigationParameters
+            {
+                { "pet", Pet }
+            };
+            await _navigationService.NavigateAsync("EditPet",parametros);
         }
     }
 }
